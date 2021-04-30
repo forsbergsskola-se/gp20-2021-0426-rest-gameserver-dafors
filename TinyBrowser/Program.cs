087d51a30f791ce274d1baa30982ef3f08078dfe
dafors <da_fors@hotmail.com>
2021-04-30 14:20:58 +0200

    Grab hrefs

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,51 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 namespace TinyBrowser
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args) {
             const string titleStartTag = "<title>";
             const string titleEndTag = "</title>";
-            var host = "acme.com";
-            var uri = "/";
+            const string hrefStartTag = "<a href=\""; // works?
+            const string hrefEndTag = "\"";
+            const string host = "acme.com";
+            const string uri = "/";
+            
             //string eol = Environment.NewLine;
             Console.WriteLine("Hello World!");
             var tcpClient = new TcpClient("acme.com", 80);
             var stream = tcpClient.GetStream();
-            var streamwriter = new StreamWriter(stream, Encoding.ASCII);
+            var streamWriter = new StreamWriter(stream, Encoding.ASCII);
             var request = $"GET {uri} HTTP/1.1\r\nHost: {host}\r\n\r\n";
-            streamwriter.Write(request);
+            streamWriter.Write(request); // add data to the buffer
+            streamWriter.Flush();        // send the buffered data
+            
             var streamReader = new StreamReader(stream);
             var response = streamReader.ReadToEnd();
-
+            
             var uriBuilder = new UriBuilder(null, host) {Path = uri};
             
             Console.WriteLine(response);
             Console.WriteLine( $"Opened {uriBuilder}");
-            string title = string.Empty;
-            
+            string title = FindTextBetweenTags(response, titleStartTag, titleEndTag);
             Console.WriteLine($"Title is: {title}");
+            Console.WriteLine($"Strings from all occurrences of '{hrefStartTag}' and '{hrefEndTag}'");
+            foreach (string s in GetTextBetweenCharsFromString(response, hrefStartTag, hrefEndTag)) {
+                Console.WriteLine(s);
+            }
         }
-        
-        
-            
-        public static string FindTextBetweenTags(string content, string startTag, string endTag) {
+
+        public static string FindTextBetweenTags(string inputText, string startTag, string endTag, int startOffset = 0) {
             string result = string.Empty;
-            var titleStartIndex = content.IndexOf(startTag);
-            if (titleStartIndex != -1) {
-                titleStartIndex += startTag.Length;
-                var titleEndIndex = content.IndexOf(endTag);
-                if (titleEndIndex > titleStartIndex) {
-                    result = content[titleStartIndex..titleEndIndex]; //range lookup
-                    //title = response.Substring(titleStartIndex , titleEndIndex - titleStartIndex );
-                }
+            var startIndex = inputText.IndexOf(startTag, startOffset);
+            if (startIndex != -1) {
+                startIndex += startTag.Length;
+                var endIndex = inputText.IndexOf(endTag, startIndex); // find next occurence of endTag
+                result = inputText[startIndex..endIndex]; //range lookup
             }
             return result;
         }
-    }
-    
 
+        static IEnumerable<string> GetTextBetweenCharsFromString(string inputText, string startTag, string endTag) {
+            int currentIndex = 0;
+            Console.WriteLine($"finding occurrences between {startTag} and {endTag}...");
+            while (true) {
+                var startIndex = inputText.IndexOf(startTag, currentIndex);
+                if (startIndex == -1)
+                    yield break;
+                startIndex += startTag.Length;
+                var endIndex = inputText.IndexOf(endTag, startIndex);
+                if (endIndex == -1)
+                    yield break;
+                yield return inputText[(startIndex)..endIndex];
+                currentIndex = endIndex;
+            }
+        }
+    }
 }
