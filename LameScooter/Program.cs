12e8ad9a65d5936b6fa820de7973066f711f920f
dafors <da_fors@hotmail.com>
2021-05-06 16:01:07 +0200

    throw argument exception whenprogram arguments contain numerals

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,25 +1,31 @@
 ﻿using System;
+using System.Linq;
 
 namespace LameScooter
 {
     class Program {
         private const string localJsonPath = "scooters.json";
+        // static async Task Main(string[] args)
         static void Main(string[] args)
         {
-            
-            Console.WriteLine(args[0]);
             //IRental rental = new OfflineLameScooterRental(localJsonPath);
             //IRental rental = new OfflineLameScooterRental();
             IRentalAsync rental = new OfflineLameScooterRental();
             //rental.Init(localJsonPath);
             rental.InitAsync(localJsonPath).GetAwaiter().GetResult();
             foreach (var arg in args) {
-                Console.WriteLine($"station {arg} has {rental.GetScooterCount(arg)}");
+                if (ContainsDigit(arg))
+                    throw new System.ArgumentException($"Program argument must not contain numerals");
+                Console.WriteLine($"station {arg} has {rental.GetScooterCountInStation(arg)}");
             }
             
             //Address address = JsonConvert.DeserializeObject<Address>(json);
             
             //var count = await rental.GetScooterCountInStation(args[0]); // Replace with command line argument.
+        }
+
+        private static bool ContainsDigit(string s) {
+            return s.Any(c => char.IsDigit(c));
         }
     }
 }
