12242bb321f1deef1f90203a24a637f4e25b63fc
dafors <da_fors@hotmail.com>
2021-05-10 16:31:03 +0200

    Add logic to request and display github user.

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,35 +1,62 @@
-﻿using System;
+﻿// microsoft docs used for this repo: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
+using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace GitHubExplorer
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-        
         static readonly HttpClient client = new HttpClient();
-        static async Task Main()
-        {
-            // Call asynchronous network methods in a try/catch block to handle exceptions.
-            try	
-            {
-                HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                // Above three lines can be replaced with new helper method below
-                // string responseBody = await client.GetStringAsync(uri);
-
-                Console.WriteLine(responseBody);
-            }
-            catch(HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");	
-                Console.WriteLine("Message :{0} ",e.Message);
-            }
+        static async Task Main(string[] args) {
+            var result = await ProcessResponse();
+            UserInfo userInfo = result.Item1;
+            MainPageUris mainPageUris = result.Item2;
+            Console.WriteLine(userInfo);
         }
+
+        private static async Task<(UserInfo, MainPageUris)> ProcessResponse(string user = "marczaku") {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "korv");
+            
+            Task<string> stringTask = client.GetStringAsync($"https://api.github.com/users/{user}");
+            //string message = await stringTask;
+            //Task<Stream> streamTask = client.GetStreamAsync($"https://api.github.com/users/{user}");
+            //UserInfo userInfo = await JsonSerializer.DeserializeAsync<UserInfo>(await streamTask);
+            //MainPageUris mainPageUris = await JsonSerializer.DeserializeAsync<MainPageUris>(await streamTask);
+            UserInfo userInfo = JsonSerializer.Deserialize<UserInfo>(await stringTask);
+            MainPageUris mainPageUris = JsonSerializer.Deserialize<MainPageUris>(await stringTask);
+            return (userInfo, mainPageUris);
+        }
+    }
+    
+    public class UserInfo {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        
+        [JsonPropertyName("company")]
+        public string Company { get; set; }
+        
+        [JsonPropertyName("location")]
+        public string Location { get; set; }
+
+        public override string ToString() {
+            return $"User: {Name} \n" +
+                   $"Company: {Company} \n" +
+                   $"Location: {Location}";
+        }
+    }
+
+    public class MainPageUris {
+        [JsonPropertyName("organizations_url")]
+        public Uri OrganizationsUrl { get; set; }
+
+        [JsonPropertyName("repos_url")]
+        public Uri ReposUrl { get; set; }
     }
 }
