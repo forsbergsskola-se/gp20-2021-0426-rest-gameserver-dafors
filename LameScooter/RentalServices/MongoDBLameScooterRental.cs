036b38bb788f50caac4ed64a3d4908e1f2d1d84d
dafors <da_fors@hotmail.com>
2021-05-26 20:38:19 +0200

    Install support for mongodb

--- LameScooter/RentalServices/MongoDBLameScooterRental.cs
+++ LameScooter/RentalServices/MongoDBLameScooterRental.cs
@@ -1,5 +1,19 @@
-﻿namespace LameScooter.RentalServices {
-    public class MongoDBLameScooterRental {
+﻿using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace LameScooter.RentalServices {
+    public class MongoDBLameScooterRental : IRental {
+        private MongoClient client;
+        public int GetScooterCountInStation(string nameOfStation) {
+            throw new System.NotImplementedException();
+        }
+
+        public void Init(string uri ="") {
+            InitAsync(uri).GetAwaiter().GetResult();
+        }
         
+        public async Task InitAsync(string uri = "") {
+            client = new MongoClient("");
+        }
     }
 }
